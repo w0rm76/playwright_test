821b3f2e02c2f37800e564ae55381f97f60e2bfc
Alexey Vorobev <chir_10@mail.ru>
2024-05-31 18:29:18 +0200

    add search coin test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,7 +17,17 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 }); */
 
-test('page is opened', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://coinmarketcap.com/');
   await expect(page).toHaveTitle('Cryptocurrency Prices, Charts And Market Capitalizations | CoinMarketCap');
 });
+
+test('search coin', async ({ page }) => {
+  //await page.locator('div[class="sc-a8231d78-1 kNerue"]').click();
+  await page.goto('https://coinmarketcap.com/');
+  await page.locator('div.sc-e20acb0c-1').click();
+  await page.getByPlaceholder('Search coin, pair, NFT, contract address, exchange, or post').fill('notcoin');
+  await page.locator('a>span', { hasText: 'Notcoin' }).click();
+
+  await expect(page).toHaveTitle('Notcoin price today, NOT to USD live price, marketcap and chart | CoinMarketCap');
+})
