bf6ae39ffd63b4354302ceecb018c9f4c46b53c8
Alexey Vorobev <chir_10@mail.ru>
2024-06-04 11:29:42 +0200

    rewrite change language tests with classes

--- coinmarketcap-com-page.ts
+++ coinmarketcap-com-page.ts
@@ -1,46 +1,56 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
 export class CoinmarketcapComPage {
-    readonly coin: string;
+    readonly coin: string | undefined;
     readonly page: Page;
-    readonly searchCoinFieldButtonDesktop: Locator;
-    readonly searchCoinField: Locator;
-    readonly searchCoinFieldItem: Locator;
-/*     readonly chooseLanguageButton: Locator;
+    private searchCoinFieldButtonDesktop: Locator;
+    private searchCoinField: Locator;
+    private searchCoinFieldItem: Locator;
+    readonly chooseLanguageButton: Locator;
     readonly chooseLanguageSearchField: Locator;
     readonly chooseLanguageDropdown: Locator;
     readonly popularLanguagesSection: Locator;
-    readonly allLanguagesSection: Locator; */
+    readonly allLanguagesSection: Locator;
+    readonly chooseLanguageDropdownItem: Locator;
 
-    constructor(page: Page, coin: string) {
+    constructor(page: Page, coin?: string) {
         this.coin = coin;
         this.page = page;
-        this.searchCoinFieldButtonDesktop = page.locator('div.sc-e20acb0c-1');
-        this.searchCoinField = page.getByPlaceholder('Search coin, pair, NFT, contract address, exchange, or post'); 
-        this.searchCoinFieldItem = page.locator(`a[href="/currencies/${coin.toLowerCase()}/"].bwRagp`);
-/*         this.chooseLanguageButton = page.locator('span.iymbsY');//('//*[@id="__next"]/div[2]/div[1]/div[1]/div[2]/div[1]/section/div/div[1]/div/div[2]/div[1]/div[1]/div');//('div.cmc-popover').filter({ hasText: /^English$/ });//(`div.cmc-popover`);//'button[title="Change your language"]');
-        this.chooseLanguageDropdown = page.locator(`div.cmc-popover__dropdown`);
-        this.popularLanguagesSection = page.locator(`div > p, {hasText: Popular languages}`);
-        this.allLanguagesSection = page.locator(`div > p, {hasText: All languages}`); */
+        this.chooseLanguageSearchField = page.locator('input[placeholder="Search"]');
+        this.chooseLanguageButton = page.locator('span.iymbsY');
+        this.chooseLanguageDropdown = page.locator('div.cmc-popover__dropdown');
+        this.popularLanguagesSection = page.locator('div > p').filter({hasText: 'Popular languages'});
+        this.allLanguagesSection = page.locator('div > p').filter({hasText: 'All languages'});
+        this.chooseLanguageDropdownItem = page.locator('a.cmc-language-picker__option');
     }
+
+
 
     async goto() {
         await this.page.goto('https://coinmarketcap.com/');
     }
 
     async openCoinPage(coin: string) {
+        this.searchCoinFieldButtonDesktop = this.page.locator('div.sc-e20acb0c-1');
+        this.searchCoinField = this.page.getByPlaceholder('Search coin, pair, NFT, contract address, exchange, or post'); 
+        this.searchCoinFieldItem = this.page.locator(`a[href="/currencies/${coin?.toLowerCase()}/"].bwRagp`);
+
         await this.searchCoinFieldButtonDesktop.click();
         await this.searchCoinField.fill(coin);
         await this.searchCoinFieldItem.click();
     }
 
-/*     async changeLanguage(language: string) {
-        await console.log(`it/'s done 1!`);
+    async findLanguageDropdown() {
         await this.chooseLanguageButton.click();
-        await console.log(`it/'s done 2!`);
-        await this.chooseLanguageSearchField.click();
-        // dropdown has 2 subsections: popular and all languages, and also has 11 languages 
-        //await expect (this.popularLanguagesSection.isVisible && this.allLanguagesSection.isVisible);
-        await console.log(`it/'s done 3!`);
-    } */
+    } 
+
+    async changeLanguageEngToRu() {
+        await this.chooseLanguageButton.click();
+        await this.chooseLanguageDropdown.locator(`a.cmc-language-picker__option > span`).filter({hasText: 'ru'}).first().click();
+    }
+
+    async changeLanguageRuToEng() {
+        await this.chooseLanguageButton.click();
+        await this.chooseLanguageDropdown.locator(`a.cmc-language-picker__option > span`).filter({hasText: 'en'}).first().click();
+    }
 }
--- tests/changeLanguage.spec.ts
+++ tests/changeLanguage.spec.ts
@@ -1,24 +1,51 @@
 import { test, expect } from '@playwright/test';
 import { CoinmarketcapComPage } from '../coinmarketcap-com-page'; 
 
-test(`change language`, async ({ page })=> {
-    page.goto('https://coinmarketcap.com/');
-    await page.locator('span.iymbsY').click();
-    await expect(page.locator('div.cmc-popover__dropdown')).toBeVisible();
-    // check sub-headers
-    await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/Popular languages/);
-    await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/All languages/);
-    // check laguages quantity
-    await expect(page.locator(`a.cmc-language-picker__option`)).toHaveCount(36);
-    // change language to russian
-    await page.locator('input[placeholder="Search"]').fill('r');
-    await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/Русский/);
-    await page.locator(`a.cmc-language-picker__option > span`).filter({hasText: "ru"}).first().click();
-    await expect(page).toHaveTitle('Цены, графики и рыночная капитализация криптовалют | CoinMarketCap');
-    // change language to english back
-    await page.locator('span.iymbsY').click();
-    await page.locator('input[placeholder="Поиск"]').fill('eng');
-    await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/English/);
-    await page.locator(`a.cmc-language-picker__option > span`).filter({hasText: "en"}).first().click();
-    await expect(page).toHaveTitle('Cryptocurrency Prices, Charts And Market Capitalizations | CoinMarketCap');
+//  === NOT PAGE OBJECT ===
+//
+// test(`change language`, async ({ page })=> {
+//     page.goto('https://coinmarketcap.com/');
+//     await page.locator('span.iymbsY').click();
+//     await expect(page.locator('div.cmc-popover__dropdown')).toBeVisible();
+//     // check sub-headers
+//     await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/Popular languages/);
+//     await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/All languages/);
+//     // check laguages quantity
+//     await expect(page.locator(`a.cmc-language-picker__option`)).toHaveCount(36);
+//     // change language to russian
+//     await page.locator('input[placeholder="Search"]').fill('r');
+//     await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/Русский/);
+//     await page.locator(`a.cmc-language-picker__option > span`).filter({hasText: "ru"}).first().click();
+//     await expect(page).toHaveTitle('Цены, графики и рыночная капитализация криптовалют | CoinMarketCap');
+//     // change language to english back
+//     await page.locator('span.iymbsY').click();
+//     await page.locator('input[placeholder="Поиск"]').fill('eng');
+//     await expect(page.locator(`div.cmc-popover__dropdown`)).toHaveText(/English/);
+//     await page.locator(`a.cmc-language-picker__option > span`).filter({hasText: "en"}).first().click();
+//     await expect(page).toHaveTitle('Cryptocurrency Prices, Charts And Market Capitalizations | CoinMarketCap');
+//   })
+
+  test('check change language button', async ({ page }) => {
+    const coinmarketcapComPage = new CoinmarketcapComPage(page);
+    await coinmarketcapComPage.goto();
+    await expect(coinmarketcapComPage.chooseLanguageButton).toBeVisible();
   })
+
+  test('check change language dropdown', async ({ page }) => {
+    const coinmarketcapComPage = new CoinmarketcapComPage(page);
+    await coinmarketcapComPage.goto();
+    await coinmarketcapComPage.findLanguageDropdown();
+    await expect(coinmarketcapComPage.chooseLanguageDropdown).toBeVisible();
+    await expect(coinmarketcapComPage.popularLanguagesSection).toBeVisible();
+    await expect(coinmarketcapComPage.allLanguagesSection).toBeVisible();
+    await expect(coinmarketcapComPage.chooseLanguageDropdownItem).toHaveCount(36);
+  });
+
+  test('check language changing', async ({ page }) => {
+    const coinmarketcapComPage = new CoinmarketcapComPage(page);
+    await coinmarketcapComPage.goto();
+    await coinmarketcapComPage.changeLanguageEngToRu();
+    await expect(page).toHaveTitle('Цены, графики и рыночная капитализация криптовалют | CoinMarketCap');
+    await coinmarketcapComPage.changeLanguageRuToEng();
+    await expect(page).toHaveTitle('Cryptocurrency Prices, Charts And Market Capitalizations | CoinMarketCap');
+  });
